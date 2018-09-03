2f715f52ed3e1eee1470d0b02292ff8029700382
Diogo Cardoso <tuniq@live.com.pt>
2018-09-03 18:36:43 +0100

    Clean up themes file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,33 +1,9 @@
 import * as styledComponents from 'styled-components';
 
-// const {
-//   default: styled,
-//   css,
-//   injectGlobal,
-//   keyframes,
-//   ThemeProvider
-// } = styledComponents as styledComponents.ThemedStyledComponentsModule<IThemeInterface>;
-
 const {
   default: styled,
   injectGlobal,
 } = styledComponents as styledComponents.ThemedStyledComponentsModule<{}>;
-
-// export interface IThemeInterface {
-//   backgroundColor: string,
-//   color0: string,
-//   color1: string,
-//   color2: string,
-//   color3: string,
-// }
-
-// export const theme = {
-//   backgroundColor: '#286DA8',
-//   color0: '#286DA8',
-//   color1: '#CD5360',
-//   color2: '#B37D4E',
-//   color3: '#438496',
-// };
 
 export const colors = {
   backgroundColor: '#286DA8',
@@ -35,4 +11,3 @@ export const colors = {
 
 export default styled;
 export { injectGlobal };
-// export { css, injectGlobal, keyframes, ThemeProvider };
