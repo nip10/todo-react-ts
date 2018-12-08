7c26e787ee2699e165b16e6eb2bb81c287764892
Diogo Cardoso <diogocardoso92@outlook.com>
2018-12-08 19:35:37 +0000

    Properly comment out comments

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -59,8 +59,8 @@ const AddTodo: React.SFC<IAddTodoProps> = props => {
 
   return (
     <Form onSubmit={onSubmitHandler}>
-      // the following weird ref line with 'as any' is caused by styled component's bad ts typings
-      // check here https://github.com/DefinitelyTyped/DefinitelyTyped/issues/28884
+      {/* the following weird ref line with 'as any' is caused by styled component's bad ts typings
+      check here https://github.com/DefinitelyTyped/DefinitelyTyped/issues/28884 */}
       <Input type="text" placeholder="Task" ref={textInput as any} />
       <Button type="submit">
         Add Todo
