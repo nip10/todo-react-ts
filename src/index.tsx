abc1dd46faec40d9f1a02e0d73ee0502e9349e74
Diogo Cardoso <diogocardoso92@outlook.com>
2019-04-12 18:18:52 +0100

    Add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import store from "./store";
+import { store, persistor } from "./store";
 
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,27 +2,26 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk, { ThunkMiddleware } from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "../reducers";
-import { loadState, saveState } from "./persistedState";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 type IAppState = ReturnType<typeof rootReducer>;
 
-const persistedState = loadState();
+const persistConfig = {
+  key: "root",
+  storage
+};
 
-const store = createStore(
-  rootReducer,
-  persistedState,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
   composeEnhancers(
     applyMiddleware(logger, thunk as ThunkMiddleware<IAppState, any>)
   )
 );
 
-store.subscribe(() => {
-  saveState(store.getState());
-});
-
-// TODO: Is there a need to unsubscribe ?
-
-export default store;
+export const persistor = persistStore(store);
