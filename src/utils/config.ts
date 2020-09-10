db591a1e012987cb31a923528e1875fff9850c8e
Diogo Cardoso <diogocardoso92@outlook.com>
2020-09-11 00:15:48 +0100

    Update api url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 const isDev = process.env.NODE_ENV === "development";
 const API_BASE_URL = isDev
   ? "http://localhost:3001"
-  : "https://www.api.todo.diogocardoso.me";
+  : "https://www.api.todo.diogocardoso.dev";
 
 const instance = axios.create({
-  baseURL: API_BASE_URL
+  baseURL: API_BASE_URL,
 });
 
 export default instance;
